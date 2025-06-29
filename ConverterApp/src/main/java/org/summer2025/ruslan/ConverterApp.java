4e1698f8751d225ecaaf87eb9d1d1c06d13ad175
JstRuslan <send2ruslan@gmail.com>
2025-06-29 14:06:46 +0300

    Initial commit

---
+++ ConverterApp/src/main/java/org/summer2025/ruslan/ConverterApp.java
@@ -0,0 +1,17 @@
+package org.summer2025.ruslan;
+
+public class ConverterApp {
+    private static final double CONV_K=2.20462;
+
+    public static void main(String[] args) {
+        System.out.println("Converter App.");
+
+        double  kgs=5;
+        double pounds=convKgsToPounds(kgs);
+        System.out.println("Result is "+pounds+" pounds.");
+    }
+
+    private static double convKgsToPounds(double kgs) {
+        return kgs*CONV_K;
+    }
+}
