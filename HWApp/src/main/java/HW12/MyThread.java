a39c0faafbc05a575c865f198715ab59b501974f
JstRuslan <send2ruslan@gmail.com>
2025-08-03 16:59:37 +0300

    Add homework 12. Threads

---
+++ HWApp/src/main/java/HW12/MyThread.java
@@ -0,0 +1,15 @@
+package HW12;
+
+public class MyThread  extends Thread{
+
+    DataHandler dataHandler;
+
+    public MyThread(String name, DataHandler dataHandler) {
+        super(name);
+        this.dataHandler = dataHandler;
+    }
+
+    public void run() {
+        dataHandler.getOutput();
+    }
+}
