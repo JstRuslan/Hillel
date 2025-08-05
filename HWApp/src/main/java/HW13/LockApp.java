c5790db7de0c69577ac1ace6b77e0109bc78cb81
JstRuslan <send2ruslan@gmail.com>
2025-08-05 04:01:00 +0300

    Add homework 13

---
+++ HWApp/src/main/java/HW13/LockApp.java
@@ -0,0 +1,14 @@
+package HW13;
+
+public class LockApp {
+
+    public static void main(String[] args) {
+        int[] numbers = new DataRepository().getData();
+        DataHandler dataHandler = new DataHandler();
+        for (int num : numbers) {
+            System.out.println("Initial value is " + num);
+            int newNum = dataHandler.modify(num);
+            System.out.println("New value is " + newNum);
+        }
+    }
+}
