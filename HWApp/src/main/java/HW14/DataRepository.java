96d3fc7cb50be4d2d4222072cc9e5c620c2c203a
JstRuslan <send2ruslan@gmail.com>
2025-08-09 04:13:10 +0300

    Add homework #14

---
+++ HWApp/src/main/java/HW14/DataRepository.java
@@ -0,0 +1,16 @@
+package HW14;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class DataRepository {
+
+    // Метод повертає список імен
+    public List<String> getData() {
+        List<String> list = new ArrayList<>();
+        Collections.addAll(list, "Alice", "Bob", "Lucy", "Denis", "Tom");
+        return list;
+    }
+}
---
+++ HWApp/src/main/java/HW14/UIOperator.java
@@ -0,0 +1,8 @@
+package HW14;
+
+public class UIOperator {
+
+    public void getOutput(String out){
+        System.out.println(out);
+    }
+}
