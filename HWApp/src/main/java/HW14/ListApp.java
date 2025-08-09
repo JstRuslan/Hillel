48c5bb54cee9e837a18b2be1aecc7de02abb04c6
JstRuslan <send2ruslan@gmail.com>
2025-08-09 04:13:10 +0300

    Add homework #14

---
+++ HWApp/src/main/java/HW14/ListApp.java
@@ -0,0 +1,12 @@
+package HW14;
+
+public class ListApp {
+    public static void main(String[] args) {
+        DataRepository repository = new DataRepository();
+        DataHandler handler = new DataHandler();
+        UIOperator uiOperator = new UIOperator();
+        uiOperator.getOutput(handler.formListOutput(repository.getData()));
+        uiOperator.getOutput(handler.formOutput(repository.getData(), 2));
+    }
+
+}
