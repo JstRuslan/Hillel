76cb0541d6e61293607d8db99d2d36334d211733
JstRuslan <send2ruslan@gmail.com>
2025-08-12 02:35:12 +0300

    Add homework #15

---
+++ HWApp/src/main/java/HW15/MapApp.java
@@ -0,0 +1,11 @@
+package HW15;
+
+public class MapApp {
+    public static void main(String[] args) {
+        DataHandler handler = new DataHandler();
+        UIOperator uiOperator = new UIOperator();
+        uiOperator.getOutput(handler.getAll());
+        uiOperator.getOutput(handler.getById(172));
+    }
+
+}
