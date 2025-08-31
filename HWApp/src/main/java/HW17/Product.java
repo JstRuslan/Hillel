7946025b902c127f326b53eea1d48f5e43d1c480
JstRuslan <send2ruslan@gmail.com>
2025-09-01 01:41:53 +0300

    Add homework #17

---
+++ HWApp/src/main/java/HW17/Product.java
@@ -0,0 +1,28 @@
+package HW17;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private String name;
+    private String category;
+    private double price;
+
+    public Product(String name, String category, double price) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
